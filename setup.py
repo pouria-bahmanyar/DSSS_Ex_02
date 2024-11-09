9b7b99a3fd03c88bd539cb0e8dd6a713965a8c60
Pooria bahmanyar <pooriabahman@gmail.com>
2024-11-09 11:01:01 +0100

    setup file modified

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DSSS_Ex_02',            # Replace with your project name
+    version='0.1',
+    packages=find_packages(),
+    description='Data Science Survival Skills Exercise 02',
+    author='Pouria Bahmanyar',
+    author_email='pouria.bahmanyar@fau.de',
+    url='https://github.com/pouria-bahmanyar/DSSS_Ex_02',  # Replace with your repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
